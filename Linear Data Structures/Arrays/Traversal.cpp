d763e9f6dbbd08f54599cc3d21089782f6d9af70
Akash <iamakashkataria18@gmail.com>
2022-08-04 15:50:09 +0530

    Traversal Of Array

---
+++ Linear Data Structures/Arrays/Traversal.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+void Traversal_Array(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    int n, arr[n];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    Traversal_Array(arr, n);
+    return 0;
+}
