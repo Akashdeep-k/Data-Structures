af61a20352074d5b9ef1357f3c045fdb8892cf86
Akash <iamakashkataria18@gmail.com>
2022-08-28 21:29:41 +0530

    To find length of a string

---
+++ Linear Data Structures/Strings/length_of_string.cpp
@@ -0,0 +1,17 @@
+//To find length of a string
+#include<iostream>
+using namespace std;
+int length_of_string(string s){
+    int c = 0;
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        c++;
+    }
+    return c;
+}
+int main(){
+    string s;
+    cin>>s;
+    cout<<length_of_string(s)<<endl;
+    return 0;
+}
