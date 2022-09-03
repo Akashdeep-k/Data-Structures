11df4f47cf1decd19704b40a7689d3b5cddf9e3d
Akash <iamakashkataria18@gmail.com>
2022-09-04 01:53:27 +0530

    Binary search using recursion

---
+++ Recursion/Binary_search.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+bool binary_search(int arr[], int size, int start, int end, int key){
+    if(start>end){
+        return false;
+    }
+    int mid = start + (end-start)/2;
+    if(arr[mid]==key){
+        return true;
+    }
+    if(arr[mid] > key){
+        return binary_search(arr, size, start, mid - 1, key);
+    }
+    else{
+        return binary_search(arr, size, mid + 1, end, key);
+    }
+}
+int main(){
+    int n, key;
+    cin>>n>>key;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    int search = binary_search(arr, n, 0, n-1, key);
+    if(search)
+    cout<<"Element found";
+    else
+    cout<<"Element not found";
+    return 0;
+}
