06a331b7171e18ecef546677bdde3c4a0a021f31
Akash <iamakashkataria18@gmail.com>
2022-11-28 00:59:24 +0530

    peak index in mountain(bitonic array)

--- Linear Data Structures/Arrays/Searching/peak_index_mountain_array.cpp
+++ Linear Data Structures/Arrays/Searching/peak_index_mountain_array.cpp
@@ -7,20 +7,17 @@
 using namespace std;
 int peak(int arr[], int n)
 {
-    int low = 0;
-    int high = n - 1;
-    int mid = low + (high - low) / 2;
-    while (low <= high)
-    {
-        if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
-            return mid;
-        else if (arr[mid] > arr[mid - 1])
-            low = mid + 1;
+    int s = 0;
+    int e = n - 1;
+    int mid = s + (e-s)/2;
+    while(s < e){
+        if(arr[mid] > arr[mid + 1])
+            e = mid;
         else
-            high = mid - 1;
-        mid = low + (high - low) / 2;
+            s = mid + 1;
+        mid = s + (e-s)/2;
     }
-    return -1;
+    return s;
 }
 int main()
 {
